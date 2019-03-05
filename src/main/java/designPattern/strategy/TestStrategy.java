38a08f2eff739eada9490cbe8ada77290ee567fb
bandx <1125234354@qq.com>
2019-03-05 10:13:36 +0800

    观察者模式

--- src/main/java/designPattern/strategy/TestStrategy.java
+++ src/main/java/designPattern/strategy/TestStrategy.java
@@ -3,8 +3,8 @@ package designPattern.strategy;
 public class TestStrategy {
 
     public static void main(String[] args){
-        OperateSub sub = new OperateSub(9, 10);
-        OperateAdd add = new OperateAdd(9, 10);
+        Operation sub = new OperateSub(9, 10);
+        Operation add = new OperateAdd(9, 10);
 
         System.out.println(sub.calculate());
         System.out.println(add.calculate());
