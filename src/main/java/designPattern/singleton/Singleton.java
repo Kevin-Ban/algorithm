f917d2c9f386eed9fc8c052fedc3e1f4cbe082c1
bandx <1125234354@qq.com>
2019-03-05 10:21:22 +0800

    单例模式

--- src/main/java/designPattern/singleton/Singleton.java
+++ src/main/java/designPattern/singleton/Singleton.java
@@ -2,20 +2,36 @@ package designPattern.singleton;
 
 public class Singleton {
 
-    // region 双重锁机制
-    // volatile保证instance对所有线程可见
+    // region 双重检测机制
+    /**
+     *  volatile保证instance对所有线程可见
+     */
     private static volatile Singleton instance;
 
     private Singleton(){
     }
 
-    public Singleton getInstance(){
+    public static Singleton getInstance(){
         if(instance == null){
-            synchronized (this){
+            synchronized (Singleton.class){
                 if(instance == null){
                     instance = new Singleton();
                 }
             }
+        }
+        return instance;
+    }
+    // endregion
+
+    // region 方法加锁机制
+
+    /**
+     *
+     * @return
+     */
+    public static synchronized Singleton getSingleton2(){
+        if(instance == null){
+            return new Singleton();
         }
         return instance;
     }
