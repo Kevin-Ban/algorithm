4249d56cac82d0e34bc395e9a147483e2bf2e3a6
bandx <1125234354@qq.com>
2019-03-07 13:09:23 +0800

    leetcode

---
+++ src/main/java/algorithm/RangeSumBST.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+public class RangeSumBST {
+
+    /**
+     * https://leetcode.com/problems/range-sum-of-bst/
+     *
+     */
+
+    public static void main(String[] args){
+
+    }
+
+    public static int rangeSumBST(TreeNode root, int L, int R){
+        // 当节点为null，返回0
+        if(root == null){
+            return 0;
+        }
+        // 当前的值小于前一个数，进入右子节点点
+        if(root.val < L){
+            return rangeSumBST(root.right, L, R);
+        }
+        // 当前的值大于后一个属，禁入左子节点
+        if(root.val > R){
+            return rangeSumBST(root.left, L, R);
+        }
+        // 当前的值大于等于L,小于等于R时，将数加起来
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+    }
+
+    /**
+     * Definition for a binary tree node.
+     *
+     */
+     class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+     }
+}
---
+++ src/main/java/algorithm/ToLowerCase.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+public class ToLowerCase {
+
+    /**
+     * https://leetcode.com/problems/to-lower-case/
+     * @param args
+     */
+
+    public static void main(String[] args){
+        String s = "AbDf";
+        System.out.println(toLowerCase(s));
+    }
+
+
+    public static String toLowerCase(String str) {
+        if(str == null){
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z'){
+                sb.append((char)(str.charAt(i) - 'A' + 'a'));
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
