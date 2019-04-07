d90f026f0950ec1cd5bdb2e783b6b208624dfae4
bandx <1125234354@qq.com>
2019-04-07 16:42:31 +0800

    cglib代理

--- src/main/java/designPattern/proxy/cglib/SayProxy.java
+++ src/main/java/designPattern/proxy/cglib/SayProxy.java
@@ -4,7 +4,9 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
-
+/**
+ * jdk实现动态代理，每一个动态代理类相当于一个aop
+ */
 public class SayProxy implements MethodInterceptor {
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
