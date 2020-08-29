cd091d2b02fd80d5c4f895813a444de9b9b1fe03
bandx <1125234354@qq.com>
2020-08-29 20:31:53 +0800

    add(分布式锁): 分布式锁的简单实现

--- src/main/java/test/CuratorTest.java
+++ src/main/java/test/CuratorTest.java
@@ -1,4 +1,26 @@
 package test;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
 public class CuratorTest {
+
+    private static String address = "127.0.0.1:2181";
+
+    private static String path = "/test1";
+
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(address, retryPolicy);
+        client.start();
+        InterProcessLock lock = new InterProcessMutex(client, path);
+        lock.acquire();
+        Thread.sleep(10000);
+        lock.release();
+        client.close();
+    }
 }
