b5e9a46511f13abd0f53e6042a80f803270ff6ef
bandx <1125234354@qq.com>
2020-09-01 18:50:47 +0800

    add(io): 序列化

--- src/main/java/test/NIOTest.java
+++ src/main/java/test/NIOTest.java
@@ -1,4 +1,8 @@
 package test;
 
 public class NIOTest {
+
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
 }
--- src/main/java/test/TestPerson.java
+++ src/main/java/test/TestPerson.java
@@ -1,4 +1,51 @@
 package test;
 
-public class TestPerson {
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class TestPerson implements Externalizable {
+
+    private static final long serialVersionUID = 123L;
+
+    private String name;
+
+    private String password;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(this.name);
+        out.writeObject(this.password);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        this.name = (String) in.readObject();
+        this.password = (String) in.readObject();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "TestPerson{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
