3c25314faaa78e8b98180dd930398f37d87b3df9
bandx <1125234354@qq.com>
2020-09-06 19:12:15 +0800

    add(io): NIO的各种测试

--- src/main/java/test/NIOTest.java
+++ src/main/java/test/NIOTest.java
@@ -1,37 +1,77 @@
 package test;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 
 public class NIOTest {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("C:\\Users\\BDX\\Desktop\\1.txt");
-        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\BDX\\Desktop\\2.txt");
-        FileChannel inputChannel = inputStream.getChannel();
-        FileChannel outChannel = outputStream.getChannel();
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        inputChannel.read(byteBuffer);
-        byteBuffer.flip();
-        int i = byteBuffer.limit();
-        while (true) {
-            i--;
-            byte b = byteBuffer.get();
-            System.out.print(b + " ");
-            if (i == 0 || b == 'a') {
-                byteBuffer.mark();
-                break;
-            }
-        }
-        outChannel.write(byteBuffer);
-        byteBuffer.reset();
-        outChannel.write(byteBuffer);
-        inputChannel.close();
-        outChannel.close();
-        inputStream.close();
-        outputStream.close();
+
+        // region test
+//        FileInputStream inputStream = new FileInputStream("C:\\Users\\BDX\\Desktop\\1.txt");
+//        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\BDX\\Desktop\\2.txt");
+//        FileChannel inputChannel = inputStream.getChannel();
+//        FileChannel outChannel = outputStream.getChannel();
+//        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+//        inputChannel.read(byteBuffer);
+//        byteBuffer.flip();
+//        int i = byteBuffer.limit();
+//        while (true) {
+//            i--;
+//            byte b = byteBuffer.get();
+//            System.out.print(b + " ");
+//            if (i == 0 || b == 'a') {
+//                byteBuffer.mark();
+//                break;
+//            }
+//        }
+//        outChannel.write(byteBuffer);
+//        byteBuffer.reset();
+//        outChannel.write(byteBuffer);
+//        inputChannel.close();
+//        outChannel.close();
+//        inputStream.close();
+//        outputStream.close();
+        // endregion
+
+        //region NIO ByteBuffer
+//        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\BDX\\Desktop\\1.txt");
+//        FileChannel fileChannel = fileOutputStream.getChannel();
+//        ByteBuffer byteBuffer = ByteBuffer.allocate(10240);
+//        Random random = new Random();
+//        for(int i = 0; i < 10000; i++) {
+//            byteBuffer.clear();
+//            for(int j = 0; j < 100; j++) {
+//                byteBuffer.put(String.valueOf(random.nextInt()).getBytes());
+//                byteBuffer.putChar('\n');
+//            }
+//            byteBuffer.flip();
+//            fileChannel.write(byteBuffer);
+//        }
+//        fileChannel.close();
+//        fileOutputStream.close();
+        //endregion
+
+        //region writer
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\BDX\\Desktop\\1.txt"), 1024);
+//        Random random = new Random();
+//        for (int i = 0; i < 10000; i++) {
+//            int data = Math.abs(random.nextInt());
+//            bufferedWriter.write(String.valueOf(data));
+//            bufferedWriter.newLine();
+//        }
+//        bufferedWriter.flush();
+//        bufferedWriter.close();
+        //endregion
+
+        //region reader
+//        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\BDX\\Desktop\\1.txt"), 1);
+//        String s = "";
+//
+//        List<Integer> list = new ArrayList<>(1024);
+//        while((s = reader.readLine()) != null) {
+//            list.add(Integer.parseInt(s));
+//            System.out.println(s);
+//        }
+        //endregion
     }
 }
--- src/main/java/test/TestInteger.java
+++ src/main/java/test/TestInteger.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * 对于强制转换来说，基本上时截取整数部分，丢弃小数部分
  */
@@ -8,8 +12,12 @@ public class TestInteger {
     public static void main(String[] args) {
         double d = 0.4;
         double d1 = 0.9;
+        Exception e = new Exception();
+        RuntimeException runtimeException;
         float f = 0.1f;
         float f1 = 0.9f;
+        List<String> list = new ArrayList<>();
+        Iterator iterator = list.iterator();
         System.out.println("d:" + (int) d);
         System.out.println("d1:" + (int) d1);
         System.out.println("f:" + (int) f);
